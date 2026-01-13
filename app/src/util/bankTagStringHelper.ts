111ae4cff3a19de0a24820cca6103c8f586dcbcd
Zane Shaw <zane@squidee.dev>
2026-01-13 13:28:57 +1100

    fix import string layout parsing

--- app/src/util/bankTagStringHelper.ts
+++ app/src/util/bankTagStringHelper.ts
@@ -89,7 +89,7 @@ export function generateItemIds(importStringArray: string[]): string[] {
 }
 
 export function indexToRowColumn(itemIndex: number, importString: string) {
-  const tokens = importString.split('layout,')[1].split(','); // [positionIndex, itemId, positionIndex, itemId, ...]
+  const tokens = importString.split('layout,').at(-1)!.split(','); // [positionIndex, itemId, positionIndex, itemId, ...]
   const positionIndex = parseInt(tokens[itemIndex * 2]);
 
   return { row: Math.floor(positionIndex / 8) + 1, column: (positionIndex % 8) + 1 };
